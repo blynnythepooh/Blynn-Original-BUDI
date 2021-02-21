7b08492f52f1deb6c707530b941d5a033eef0e4a
Blynn Shideler <b.shideler@yahoo.com>
2021-02-21 15:20:46 -0800

    Initial Commit

---
+++ TransferDataPhoneWatchFeb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransferDataPhoneWatchFeb
+//
+//  Created by Blynn Shideler on 2/21/21.
+//  Copyright © 2021 Blynn L Shideler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
